248023694ecd69a616dff74220dd0e8a48f466c4
mayingbing <mayingbing@163.com>
2016-03-23 23:19:51 +0800

    爱车app

---
+++ aiche/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  aiche
+//
+//  Created by mayingbing on 16/3/23.
+//  Copyright © 2016年 aiche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
